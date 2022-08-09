ca99074441da8d7fd03b3a1f4d72053bb0b88d9a
jaafarfares <jaafarferes@gmail.com>
2022-08-09 12:43:53 +0100

    test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,8 @@
--- sql for the airbnb
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT select ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- this is a comment 
+-- this is a second comment
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost';
+GRANT select ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;

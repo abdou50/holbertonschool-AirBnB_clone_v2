fb404361054f4527249cf271062679013535b4e0
abdou50 <abdelkaderraddadi988@gmail.com>
2022-08-09 13:27:40 +0300

    the test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- sql for the airbnb
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT select ON performance_schema.* TO 'hbnb_test'@'localhost';

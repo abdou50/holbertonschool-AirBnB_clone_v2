72a288a636c58876ec9bceb0e1534cbcf83f1263
jaafarfares <jaafarferes@gmail.com>
2022-08-10 10:43:22 +0100

    Update FileStorage with: all and delete functions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.user import User
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 
 
 class FileStorage:

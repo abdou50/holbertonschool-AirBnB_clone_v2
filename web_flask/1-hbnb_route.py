c5bdb2574df1746e5bf8018c57d00c2fb1e582b6
abdou50 <gadourraddadi@gmail.com>
2022-09-11 01:37:50 +0300

    l

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """display hbnb"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hello():
     """display hbnb"""
-    return "HBNB”
+    return 'HBNB'
 
 
 if __name__ == "__main__":

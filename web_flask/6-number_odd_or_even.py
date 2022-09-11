6d4bd2f4061758d6cba0675568dd7028bcc152cb
abdou50 <gadourraddadi@gmail.com>
2022-09-11 22:35:40 +0300

    :

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""using flask"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """display hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hellob():
+    """display hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def helloc(text):
+    txt = text.replace('_', ' ')
+    return 'C {}'.format(txt)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hellod(text='is cool'):
+    txt = text.replace('_', ' ')
+    return 'Python {}'.format(txt)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hellon(n):
+    if type(n) is int:
+        return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def hellonn(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def hellonnn(n):
+    return render_template('6-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
